9524fafd8cc592ce025d6f282971beb611eabeb8
Sandipan Saha <69733177+volstice@users.noreply.github.com>
2023-05-12 22:45:43 +0530

    Update HandTrackModule.py

--- HandTrackModule.py
+++ HandTrackModule.py
@@ -29,7 +29,7 @@ class handDetector():
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
-#---------------------------------------------------------Finger Coordinates-------------------------------------    
+#----------------------------------------------------Finger Coordinates-------------------------------------    
     def findPosition(self, img, handNo=0, draw=True):
         xList = []
         yList = []
@@ -39,12 +39,10 @@ class handDetector():
             myHand = self.results.multi_hand_landmarks[handNo]
 
             for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
                 yList.append(cy)
-                # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 # if draw:
                 #     cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
@@ -57,7 +55,7 @@ class handDetector():
                               (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
 
         return self.lmList, bbox
-
+#-------------------------------------------------Check if the fingers are up----------------------------------
     def fingersUp(self):
         fingers = []
         # Thumb
@@ -72,6 +70,8 @@ class handDetector():
             else:
                 fingers.append(0)
         return fingers
+
+#-----------------------------------------------
 
     def findDistance(self, p1, p2, img, draw=True):
         x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
